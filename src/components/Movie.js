85a6c01037ba0b4b0a44595acf5e67173ca47c6e
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-12-31 11:47:24 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,27 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+ let id=props.id;
+  const handleDelete=async ()=>{
+    
+    let res=await fetch(`https://crudcrud.com/api/76261c11e1e5403093c6e493895a52a5/movies/`+id, {
+      method: 'DELETE'
+    }).then((res)=>{
+      if(res.ok){
+        props.getMovies();
+      }
+      else
+      throw new Error("Something went wrong");
+    })
+    
+  }
+  
     return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button className={classes.delete} onClick={handleDelete}>Delete</button>
     </li>
   );
 };
